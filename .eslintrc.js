39410b3bd634fe45ea786ae48cbc19cfdf0b9326
潘静怡 <1003346758@qq.com>
2022-08-09 04:00:57 +0800

    feat(main): 解决prettier和eslint冲突

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-essential",
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
     //"@vue/typescript/recommended",
-    "prettier",
+    'prettier',
     //"@vue/prettier/@typescript-eslint",
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
-    requireConfigFile: false,
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
